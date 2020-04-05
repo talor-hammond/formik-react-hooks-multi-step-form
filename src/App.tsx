43f9778ef88c64c58fc02d041d8e8cc773c0b865
Talor H <talorjhammond@gmail.com>
2020-04-05 17:49:55 +1200

    set up "/" and "/results" routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+// Lib
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Pages
 
 const App = () => {
-  return <h1>hi</h1>;
+  return (
+    <Router>
+      {/* TODO: header */}
+      <Switch>
+        <Route path="/">todo</Route>
+        <Route path="/results">todo</Route>
+      </Switch>
+      {/* TODO: footer */}
+    </Router>
+  );
 };
 
 export default App;
