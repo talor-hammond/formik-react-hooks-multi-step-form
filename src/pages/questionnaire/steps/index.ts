4c4be3fb5fbd142e2c013a0ff81ffb431dd8495a
Talor H <talorjhammond@gmail.com>
2020-04-12 13:54:40 +1200

    add questions/steps/index.ts w/ base logic

---
+++ src/pages/questionnaire/steps/index.ts
@@ -0,0 +1,45 @@
+// Components & schema
+
+// Other types:
+import { FormikValues } from "formik";
+
+export type Step = {
+  /** Identifies the name of the step & key in initialValues */
+  name: string;
+  component: any; // TODO: type these
+  validationSchema: any; // TODO
+};
+
+/** Considered the maximum possible steps for a user -- optionally perform filtering logic in `generateSteps` if you need to */
+const baseSteps: Step[] = [
+  {
+    name: "test1",
+    component: null,
+    validationSchema: null
+  },
+  {
+    name: "test2",
+    component: null,
+    validationSchema: null
+  }
+];
+
+export const generateSteps = (): Step[] => {
+  // Depending on the type of user, and the requirements for the form that are needed -- you can perform filtering logic here to omit steps that aren't
+  // necessary for each case
+  return baseSteps;
+};
+
+export const generateInitialValues = (filteredSteps: Step[]): FormikValues => {
+  // These initial values are assumed to be `null`.
+  // In more complex cases you may want to populate these values w/ data from an API, so you could do what you need to do in here
+  const initialValues = filteredSteps.reduce((values, step) => {
+    return { ...values, [step.name]: null };
+  }, {});
+
+  return initialValues;
+};
+
+export const getStepSchema = (currentIndex: number, steps: Step[]) => {
+  return steps[currentIndex].validationSchema;
+};
