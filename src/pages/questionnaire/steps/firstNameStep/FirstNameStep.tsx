03035667c3334d21c8f0264b741aed6f77a0c20c
Talor H <talorjhammond@gmail.com>
2020-07-12 23:52:39 +1200

    finish initial FirstNameStep

--- src/pages/questionnaire/steps/firstNameStep/FirstNameStep.tsx
+++ src/pages/questionnaire/steps/firstNameStep/FirstNameStep.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // Components
-import { Input } from "@material-ui/core";
+import { Input, InputLabel } from "@material-ui/core";
 // Types
 import { FormikProps, FormikValues } from "formik";
 // Util
@@ -14,6 +14,7 @@ type FirstNameProps = {
 const FirstName = ({ form, name }: FirstNameProps) => {
   return (
     <>
+      <InputLabel>What's your first name?</InputLabel>
       <Input {...getInputProps(name, form)} />
     </>
   );
